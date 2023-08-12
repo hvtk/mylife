bb0be406058e526d9fa557cea30f10859ce00653
hvtk <hvtk1968@gmail.com>
2023-08-12 15:49:36 +0200

    Started with the view from the homepage with bootstrap

--- app/page.jsx
+++ app/page.jsx
@@ -9,34 +9,44 @@ import selectImageFamily from 'public/assets/images/selections/family.jpg'
 
 export default function Homepage() {
   return (
-   
-      <div>
-        
+    <div className="container bg-body">
+      <div className="row">
+        <div className="col-sm-1 bg-secondary">
           <SidebarBase/>
-       
-        <div >
-              <Selection 
+        </div>
+        <div className="col-sm-11 bg-success">
+          <div className="row">
+            <div className="col-sm-4 bg-warning">
+            </div>
+            <div className="row">
+              <div className="col-3">
+                <Selection 
                 src={selectImageFamily}
-                alt="image respresending my family and friends"
-              />
-        </div>
-        <div  >
-          <SelectionName >
-            Family and Friends
-          </SelectionName>
-        </div>
-        <div >
-          <Selection
-            src={selectImageWhereILive} 
-            alt="images represending my place to live"
-          /> 
-        </div>
-        <div >
-          <SelectionName>
-            Where I live
-          </SelectionName>
+                  alt="image respresending my family and friends"
+                />
+              </div>
+              <div className="col-3">
+                <Selection
+                src={selectImageWhereILive} 
+                  alt="images represending my place to live"
+                /> 
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-3">
+                <SelectionName >
+                  Family and Friends
+                </SelectionName>
+              </div>
+              <div className="col-3">
+                <SelectionName>
+                  Where I live
+                </SelectionName>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-    
+    </div>
   )
 }
