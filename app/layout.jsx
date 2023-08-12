2482d1ea5901cbd838b7eef785988dd98aa6b497
hvtk <hvtk1968@gmail.com>
2023-08-12 11:08:47 +0200

    Added bootstrap for css and grid and flexbox. All grid code deleted.

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,3 +1,5 @@
+
+
 export const metadata = {
     title: "MyLife",
     description: "All info about my life on one place"
--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,5 @@
-import styles from '../styles/homepage.modules.css'
+import 'bootstrap/dist/css/bootstrap.css'
+
 import { Selection } from 'components/selections/Selection'
 import { SelectionName } from 'components/selections/SelectionName'
 import { SidebarBase } from 'components/sidebar/SidebarBase'
@@ -9,68 +10,32 @@ import selectImageFamily from 'public/assets/images/selections/family.jpg'
 export default function Homepage() {
   return (
    
-      <div className={styles.homepageLayout}>
+      <div>
         
           <SidebarBase/>
-        
-        <div id={styles.dialog}></div>
-        <div id={styles.figureFamily}>
+       
+        <div >
               <Selection 
                 src={selectImageFamily}
                 alt="image respresending my family and friends"
               />
         </div>
-        <div id={styles.figcaptionFamily}>
-          <SelectionName>
+        <div  >
+          <SelectionName >
             Family and Friends
           </SelectionName>
         </div>
-        <div id={styles.figureHome}>
+        <div >
           <Selection
             src={selectImageWhereILive} 
             alt="images represending my place to live"
           /> 
         </div>
-        <div id={styles.figcaptionHome}>
+        <div >
           <SelectionName>
             Where I live
           </SelectionName>
         </div>
-        <div id={styles.figureEatAndDrink}></div>
-        <div id={styles.figcaptionEatAndDrink}></div>
-      
-        <div id={styles.figureMySchedule}></div>
-        <div id={styles.figcaptionMySchedule}></div>
-      
-        <div id={styles.figurePersonalSituation}></div>
-        <div id={styles.figcaptionPersonalSituation}></div>
-      
-        <div id={styles.figureFinance}></div>
-        <div id={styles.figcaptionFinance}></div>
-      
-        <div id={styles.figureInsurance}></div>
-        <div id={styles.figcaptionInsurance}></div>
-      
-        <div id={styles.figureSleep}></div>
-        <div id={styles.figcaptionSleep}></div>
-      
-        <div id={styles.figureSubscribtions}></div>
-        <div id={styles.figcaptionSubscribtions}></div>
-      
-        <div id={styles.figureInstalledApps}></div>
-        <div id={styles.figcaptionInstalledApps}></div>
-      
-        <div id={styles.figureUsedWebsites}></div>
-        <div id={styles.figcaptionUsedWebsites}></div>
-      
-        <div id={styles.figureReminders}></div>
-        <div id={styles.figcaptionReminders}></div>
-      
-        <div id={styles.figureRightsAndObligations}></div>
-        <div id={styles.figcaptionRightsAndObligations}></div>
-      
-        <div id={styles.figureDailyRoutine}></div>
-        <div id={styles.figcaptionDailyRoutine}></div>
       </div>
     
   )
