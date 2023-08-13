9c28a4a44e02908cb6f99062aea9da16f8501487
hvtk <hvtk1968@gmail.com>
2023-08-13 11:19:44 +0200

    Added the selection pictures and other info for the homepage. Not ready yet.

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,23 @@
 import 'bootstrap/dist/css/bootstrap.css'
 
+import { MylifeImage } from '@/components/homepage/MylifeImage'
 import { Selection } from 'components/selections/Selection'
 import { SelectionName } from 'components/selections/SelectionName'
 import { SidebarBase } from 'components/sidebar/SidebarBase'
 import selectImageWhereILive from 'public/assets/images/selections/where i live.jpg'
 import selectImageFamily from 'public/assets/images/selections/family.jpg'
-
+import selectImageEatAndDrink from 'public/assets/images/selections/eat and drink.jpg'
+import selectImageSleep from 'public/assets/images/selections/sleep.jpg'
+import selectImagePersonalSituation from 'public/assets/images/selections/personal situation.jpg'
+import selectImageDailyRoutine from 'public/assets/images/selections/daily routine.jpg'
+import selectImageMyFinance from 'public/assets/images/selections/my finances.jpg'
+import selectImageInsurance from 'public/assets/images/selections/insurance.jpg'
+import selectImageSubcriptions from 'public/assets/images/selections/subcriptions.jpg'
+import selectImageInstalledApps from 'public/assets/images/selections/installed apps.jpg'
+import selectImageUsedWebsites from 'public/assets/images/selections/used websites.jpg'
+import selectImageReminders from 'public/assets/images/selections/reminders.jpg'
+import selectImageRightsAndObligations from 'public/assets/images/selections/rights and obligations.jpg'
+import selectImageMySchedule from 'public/assets/images/selections/my schedule.jpg'
 
 export default function Homepage() {
   return (
@@ -17,6 +29,13 @@ export default function Homepage() {
         <div className="col-sm-11 bg-success">
           <div className="row">
             <div className="col-sm-4 bg-warning">
+              <p>
+                Everything that influences who you are, what you can do your daily activities, organized in one place
+              </p>
+              <MylifeImage/>
+              <p>
+                In order to be able to manage everything that life demands as well as possible
+              </p>
             </div>
             <div className="col-sm-8">
               <div className="row">
@@ -34,14 +53,14 @@ export default function Homepage() {
                 </div>
                 <div className="col-3">
                   <Selection 
-                  src={selectImageFamily}
-                    alt="image respresending my family and friends"
+                  src={selectImageEatAndDrink}
+                    alt="image respresending what I eat and drink"
                   />
                 </div>
                 <div className="col-3">
                   <Selection
-                  src={selectImageWhereILive} 
-                    alt="images represending my place to live"
+                  src={selectImagePersonalSituation} 
+                    alt="images represending on my personal situation"
                   /> 
                 </div>
               </div>
@@ -58,134 +77,135 @@ export default function Homepage() {
                 </div>
                 <div className="col-3">
                   <SelectionName >
-                    Family and Friends
+                    Eat and Drink
                   </SelectionName>
                 </div>
                 <div className="col-3">
                   <SelectionName>
-                    Where I live
+                    Personal Situation
                   </SelectionName>
                 </div>
               </div>
               <div className="row">
                 <div className="col-3">
                   <Selection 
-                  src={selectImageFamily}
-                    alt="image respresending my family and friends"
+                  src={selectImageSleep}
+                    alt="image respresending what my sleep routine is"
                   />
-                </div>
-                <div className="col-3">
-                  <Selection
-                  src={selectImageWhereILive} 
-                    alt="images represending my place to live"
-                  /> 
                 </div>
                 <div className="col-3">
                   <Selection 
-                  src={selectImageFamily}
-                    alt="image respresending my family and friends"
+                  src={selectImageDailyRoutine}
+                    alt="image respresending on my daily routine"
                   />
                 </div>
                 <div className="col-3">
                   <Selection
-                  src={selectImageWhereILive} 
-                    alt="images represending my place to live"
+                  src={selectImageMyFinance} 
+                    alt="images represending my finance"
+                  /> 
+                </div>
+                
+                <div className="col-3">
+                  <Selection
+                  src={selectImageInsurance} 
+                    alt="images represending my insurances"
                   /> 
                 </div>
               </div>
               <div className="row">
                 <div className="col-3">
                   <SelectionName >
-                    Family and Friends
+                    Sleep Routine
                   </SelectionName>
                 </div>
                 <div className="col-3">
                   <SelectionName>
-                    Where I live
+                    Daily Routine
                   </SelectionName>
                 </div>
                 <div className="col-3">
                   <SelectionName >
-                    Family and Friends
+                    Finance
                   </SelectionName>
                 </div>
                 <div className="col-3">
                   <SelectionName>
-                    Where I live
-                  </SelectionName>
-                </div>
-              </div>
-              <div className="row">
-                <div className="col-3">
-                  <Selection 
-                  src={selectImageFamily}
-                    alt="image respresending my family and friends"
-                  />
-                </div>
-                <div className="col-3">
-                  <Selection
-                  src={selectImageWhereILive} 
-                    alt="images represending my place to live"
-                  /> 
-                </div>
-                <div className="col-3">
-                  <Selection 
-                  src={selectImageFamily}
-                    alt="image respresending my family and friends"
-                  />
-                </div>
-                <div className="col-3">
-                  <Selection
-                  src={selectImageWhereILive} 
-                    alt="images represending my place to live"
-                  /> 
-                </div>
-              </div>
-              <div className="row">
-                <div className="col-3">
-                  <SelectionName >
-                    Family and Friends
-                  </SelectionName>
-                </div>
-                <div className="col-3">
-                  <SelectionName>
-                    Where I live
-                  </SelectionName>
-                </div>
-                <div className="col-3">
-                  <SelectionName >
-                    Family and Friends
-                  </SelectionName>
-                </div>
-                <div className="col-3">
-                  <SelectionName>
-                    Where I live
+                    Insurances
                   </SelectionName>
                 </div>
               </div>
               <div className="row">
                 <div className="col-3">
                   <Selection 
-                  src={selectImageFamily}
-                    alt="image respresending my family and friends"
+                  src={selectImageSubcriptions}
+                    alt="image respresending my subcriptions"
                   />
                 </div>
                 <div className="col-3">
                   <Selection
-                  src={selectImageWhereILive} 
-                    alt="images represending my place to live"
+                  src={selectImageUsedWebsites} 
+                    alt="images represending my used websites"
+                  /> 
+                </div>
+                <div className="col-3">
+                  <Selection 
+                  src={selectImageInstalledApps}
+                    alt="image respresending my installed apps"
+                  />
+                </div>
+                <div className="col-3">
+                  <Selection
+                  src={selectImageReminders} 
+                    alt="images represending my reminders"
                   /> 
                 </div>
               </div>
               <div className="row">
                 <div className="col-3">
                   <SelectionName >
-                    Family and Friends
+                    Subcriptions
                   </SelectionName>
                 </div>
                 <div className="col-3">
                   <SelectionName>
-                    Where I live
+                    Used Websites
+                  </SelectionName>
+                </div>
+                <div className="col-3">
+                  <SelectionName >
+                    Installed Apps
+                  </SelectionName>
+                </div>
+                <div className="col-3">
+                  <SelectionName>
+                    Reminders
+                  </SelectionName>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-3">
+                  <Selection 
+                  src={selectImageRightsAndObligations}
+                    alt="image respresending my rights and obligations"
+                  />
+                </div>
+                <div className="col-3">
+                  <Selection
+                  src={selectImageMySchedule} 
+                    alt="images represending my schedule"
+                  /> 
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-3">
+                  <SelectionName >
+                    Rights and Obligations
+                  </SelectionName>
+                </div>
+                <div className="col-3">
+                  <SelectionName>
+                    My Schedule
                   </SelectionName>
                 </div>
               </div>
---
+++ components/homepage/MylifeImage.jsx
@@ -0,0 +1,56 @@
+import Image from 'next/image'
+import styles from './mylifeImageStyles.module.css'
+import MylifeLogoImageFill from 'public/assets/images/mylife-logo/life.jpg'
+import MylifeLogoImageTopLeft from 'public/assets/images/mylife-logo/home.jpg'
+import MylifeLogoImageTopRight from 'public/assets/images/mylife-logo/sad.jpg'
+import MylifeLogoImageBottomRight from 'public/assets/images/mylife-logo/working.jpg'
+import MylifeLogoImageBottomLeft from 'public/assets/images/mylife-logo/happy.jpg'
+
+export function MylifeImage() {
+  return (
+    <div className={styles.divLogo} >
+      <div>
+        <Image
+          src={MylifeLogoImageFill}
+          alt="image that fills the parent element"
+          placeholder='blur'
+          quality={100}
+          fill
+          sizes="100vw"
+        />
+      </div>
+      <div className={styles.mylifeLogoTL} >
+        <Image
+          src={MylifeLogoImageTopLeft}
+          alt="image on top left in the parent element"
+          width={25}
+          height={30}
+        />
+      </div>
+      <div className={styles.mylifeLogoTR} >
+        <Image
+          src={MylifeLogoImageTopRight}
+          alt="image on top right in the parent element"
+          width={23}
+          height={32}
+        />
+      </div>
+      <div className={styles.mylifeLogoBR} >
+        <Image
+          src={MylifeLogoImageBottomRight}
+          alt="image on bottom right in the parent element"
+          width={34}
+          height={30}
+        />
+      </div>
+      <div className={styles.mylifeLogoBL} >
+        <Image
+          src={MylifeLogoImageBottomLeft}
+          alt="image on bottom left in the parent element"
+          width={29}
+          height={34}
+        />
+      </div>
+    </div>
+  )
+}
