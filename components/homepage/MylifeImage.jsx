27794b4fbf9f8f23943f7853d66e607f5533fe07
hvtk <hvtk1968@gmail.com>
2023-08-17 09:54:29 +0200

    Made MyLifeImage with bootstrapCSS.

--- components/homepage/MylifeImage.jsx
+++ components/homepage/MylifeImage.jsx
@@ -1,103 +1,52 @@
 import Image from 'next/image'
-import styles from './mylifeImageStyles.module.css'
 import MylifeLogoImageFill from 'public/assets/images/mylife-logo/life.jpg'
 import MylifeLogoImageTopLeft from 'public/assets/images/mylife-logo/home.jpg'
 import MylifeLogoImageTopRight from 'public/assets/images/mylife-logo/sad.jpg'
 import MylifeLogoImageBottomRight from 'public/assets/images/mylife-logo/working.jpg'
 import MylifeLogoImageBottomLeft from 'public/assets/images/mylife-logo/happy.jpg'
 
-// export function MylifeImage() {
-//   return (
-//     <div className={styles.divLogo} >
-//       <div>
-//         <Image
-//           src={MylifeLogoImageFill}
-//           alt="image that fills the parent element"
-//           placeholder='blur'
-//           quality={100}
-//           fill
-//           sizes="100vw"
-//         />
-//       </div>
-//       <div className={styles.mylifeLogoTL} >
-//         <Image
-//           src={MylifeLogoImageTopLeft}
-//           alt="image on top left in the parent element"
-//           width={25}
-//           height={30}
-//         />
-//       </div>
-//       <div className={styles.mylifeLogoTR} >
-//         <Image
-//           src={MylifeLogoImageTopRight}
-//           alt="image on top right in the parent element"
-//           width={23}
-//           height={32}
-//         />
-//       </div>
-//       <div className={styles.mylifeLogoBR} >
-//         <Image
-//           src={MylifeLogoImageBottomRight}
-//           alt="image on bottom right in the parent element"
-//           width={34}
-//           height={30}
-//         />
-//       </div>
-//       <div className={styles.mylifeLogoBL} >
-//         <Image
-//           src={MylifeLogoImageBottomLeft}
-//           alt="image on bottom left in the parent element"
-//           width={29}
-//           height={34}
-//         />
-//       </div>
-//     </div>
-//   )
-// }
-
 export function MylifeImage() {
   return (
     <div className="border border-dark border-1 w-200px h-200px">
-          
-      <div className={styles.divLogo}>
+      <div className="position-relative">
         <Image 
           src={MylifeLogoImageFill}
           alt="image that fills the parent element"
           width={200}
           height={200}
         />
-      </div>
-      <div >
-        <Image
-          src={MylifeLogoImageTopLeft}
-          alt="image on top left in the parent element"
-          width={25}
-          height={30}
-        />
-      </div>
-      <div >
-        <Image
-          src={MylifeLogoImageTopRight}
-          alt="image on top right in the parent element"
-          width={23}
-          height={32}
-        />
-      </div>
-      <div >
-        <Image
-          src={MylifeLogoImageBottomRight}
-          alt="image on bottom right in the parent element"
-          width={34}
-          height={30}
-        />
-      </div>
-      <div  >
-        <Image
-          src={MylifeLogoImageBottomLeft}
-          alt="image on bottom left in the parent element"
-          width={29}
-          height={34}
-        />
+        <div className="position-absolute top-0 start-0">
+          <Image
+            src={MylifeLogoImageTopLeft}
+            alt="image on top left in the parent element"
+            width={65}
+            height={75}
+          />
+        </div>
+        <div className="position-absolute top-0 end-0">
+          <Image
+            src={MylifeLogoImageTopRight}
+            alt="image on top right in the parent element"
+            width={60}
+            height={90}
+          />
+        </div>
+        <div className="position-absolute bottom-0 end-0">
+          <Image
+            src={MylifeLogoImageBottomRight}
+            alt="image on bottom right in the parent element"
+            width={90}
+            height={80}
+          />
+        </div>
+        <div className="position-absolute bottom-0 start-0" >
+          <Image
+            src={MylifeLogoImageBottomLeft}
+            alt="image on bottom left in the parent element"
+            width={77}
+            height={90}
+          />
+        </div>
       </div>
     </div>
   )
