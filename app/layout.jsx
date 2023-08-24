0ea7506ef5bcca24331ad2f45635f81bc55ee307
hvtk <hvtk1968@gmail.com>
2023-08-24 15:24:36 +0200

    Made it possible to use custom scss files. Using bootstap JS is not fixed yet!

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,9 +1,32 @@
-import 'bootstrap/dist/css/bootstrap.css'
+//To use bootstrap styles without customization
+// import 'bootstrap/dist/css/bootstrap.min.css'
+'use client'
 
-export const metadata = {
-    title: "MyLife",
-    description: "All info about my life on one place",
+import '../styles/customBootstrap.scss'
+
+import '../styles/globals.css'
+
+//To use bootstrap styles with customization
+// export function MyApp({ Component, pageProps}) {
+
+//     return <Component {...pageProps} />
+// }
+
+//To use bootstrap with JS
+import { useEffect } from 'react'
+
+export function MyApp({ Component, pageProps }) {
+    useEffect(() => {
+        import('bootstrap/dist/js/bootstrap')
+    }, [])
+
+    return <Component {...pageProps} />
 }
+
+// export const metadata = {
+//     title: "MyLife",
+//     description: "All info about my life on one place",
+// }
 
 export default function RootLayout({ children }) {
     return (
--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {}
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+const path = require('path')
+
+module.exports = {
+    sassOptions: {
+        includePaths: [path.join(__dirname, 'styles')],
+    },
+}
+
+
