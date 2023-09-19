db45fb9d9cd1d888eccd203a2c8fb8db67725e70
hvtk <hvtk1968@gmail.com>
2023-09-19 14:14:17 +0200

    Added prisma setup and create database table

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,32 +1,44 @@
 import NextAuth from "next-auth"
-import { authenticate } from "@/services/authService"
+// import { authenticate } from "@/services/authService"
 import CredentialsProvider from "next-auth/providers/credentials"
 
 export const authOptions = {
     providers: [
-        CredentialsProvider = {
+        CredentialsProvider({
+            // the name to display on the sign in form (e.g.'Sign in with...')
             name: 'Credentials',
+            // The credentials is used to generate a suitable form on the sign in page.
+            // You can specify whatever fields you are expecting to be submitted.
+            // e.g. domain, username, password, 2FA token, etc.
+            // You can pass any HTML attribute to the <input> tag through the object.
             credentials: {
-                email: { label: "Email", type: "text" },
+                username: { label: "Username", type: "text", placeholder: "full name"},
                 password: { label: "Password", type: "password"}
             },
+
             async authorize (credentials, req) {
-                if (typeof credentials !== "undefined") {
-                    const res = await authenticate(credentials.email, credentials.password )
-                    if (typeof res !== "undefined") {
-                        return { ...res.user, apiToken: res.token }
-                    } else {
-                        return null
-                    }
-                } else {
-                    return null
-                }
-            }
-        }
+               // You need to provide your own logic here to look up the user from the credentials supplied
+               const user = {id: "1", name: "J Smith", email: "jsmith@example.com"}
+
+               if (user) {
+                // Any object returned will be savedin 'user' property of the JWT
+                return user
+               } else {
+                // If you return null then an error will be displayed advising the user to check their details
+                return null
+               }
+            },
+
+
+
+            // // To tell te application to use nextauth_secret, see the next.config.js file
+            // secret: process.env.NEXTAUTH_SECRET,
+        })
     ],
-    session: { strategy: "jwt"}
+    // session: { strategy: "jwt"}
 }
 
+// Route handlers is the preferred way to handle REST-like requests in App Router
 const handler = NextAuth(authOptions)
 
 export { handler as GET, handler as POST }
--- next.config.js
+++ next.config.js
@@ -7,13 +7,25 @@
 const path = require('path')
 
 module.exports = {
+
     sassOptions: {
-        includePaths: [path.join(__dirname, 'styles')],
+        includePaths: [path.join(__dirname, 'styles')]
     },
 
-    experimental: {
-        serverActions: true,
-    },
+    // experimental: {
+    //     serverActions: true
+    // },
+
+    // // For highlighting potential problems in the application
+    // reactStrictMode: true,
+
+    // // Is an Rust-based platform for fast developer tools. It can be used for both compilation and buncling
+    // swcMinify: true,
+
+    // // 
+    // env: {
+    //     NEXTAUTH_SECRET: '3P7DEueFGZmo/bdXo/hmAOH5QYdACe5jmRe27F2Z9N0='
+    // }
 }
 
 
